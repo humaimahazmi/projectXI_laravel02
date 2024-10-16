27aa875b0f877cc3ecdcb40d609614f2517033bd
humaimahazmi <541231093@student.smktelkom-pwt.sch.id>
2024-10-16 14:19:36 +0700

    add index and login page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index'); // merujuk ke file welcome.blade.
+});
+
+Route::get('/welcome', function () {
+    return view('welcome'); // merujuk ke file welcome.blade.
+});
+
+Route::get('/login', function () {
+    return view('login'); // merujuk ke file welcome.blade.
 });
